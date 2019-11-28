e43afbb66e491065a8fd90ae8ef82685a0eb67fa
chxie <chxie@haivera.com>
2019-11-28 16:10:59 +0800

    添加nacos配置，可以在nacas配置中心添加配置，微服务将获取默认与spring.application.name相同的名称的配置

--- server-consumer/src/main/java/com/example/serverconsumer/serverconsumer/controller/ConsumerCtr.java
+++ server-consumer/src/main/java/com/example/serverconsumer/serverconsumer/controller/ConsumerCtr.java
@@ -2,12 +2,15 @@ package com.example.serverconsumer.serverconsumer.controller;
 
 import com.example.serverconsumer.serverconsumer.service.ConsumerCellProviderSer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RefreshScope
 @RequestMapping("/consumer")
 public class ConsumerCtr {
 
@@ -16,6 +19,12 @@ public class ConsumerCtr {
 
     @Autowired
     private ConsumerCellProviderSer consumerCellProviderSer;
+
+    /**
+     * 获取nacos配置中心 配置config.testLabel的值
+     */
+    @Value("${config.test.label:}")
+    private String testLabel;
 
     @GetMapping("/templateCell")
     public String templateCell(){
@@ -30,6 +39,15 @@ public class ConsumerCtr {
     @GetMapping("/feignCell")
     public String feignCell(){
         return consumerCellProviderSer.cellSayHello();
+    }
+
+    /**
+     * 获取Nacos注册中心配置信息
+     * @return
+     */
+    @GetMapping("/getNacosConfig")
+    public String getNacosConfig(){
+        return testLabel;
     }
 
 }
--- server-provider/src/main/java/com/example/serverprovider/controller/ProviderCtr.java
+++ server-provider/src/main/java/com/example/serverprovider/controller/ProviderCtr.java
@@ -2,20 +2,39 @@ package com.example.serverprovider.controller;
 
 import com.example.serverprovider.service.ProviderSer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/provider")
+@RefreshScope
 public class ProviderCtr {
 
     @Autowired
     private ProviderSer providerSer;
 
+    /**
+     * 获取nacos配置中心 配置config.testLabel的值
+     * 注意‘:’ 不能少
+     */
+    @Value("${config.testLabel:}")
+    private String testLabel;
+
     @GetMapping("/sayHello")
     public String sayHello(){
         return providerSer.sayHello();
+    }
+
+    /**
+     * 获取Nacos注册中心配置信息
+     * @return
+     */
+    @GetMapping("/getNacosConfig")
+    public String getNacosConfig(){
+        return testLabel;
     }
 
 }
